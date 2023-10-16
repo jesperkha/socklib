7ff3bae7792f3af5c01a2ffb6b5360d1fead24a8
jesperkha <hammerjesp@gmail.com>
2023-10-16 22:42:47 +0200

    updated build

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ if [ -z "$1" ]
     then
         echo "build [example dir]"
     else
-        # gcc examples/$1/client.c src/*.c -Iinclude $LINK -o bin/client
+        gcc examples/$1/client.c src/*.c -Iinclude $LINK -o bin/client
         gcc examples/$1/server.c src/*.c -Iinclude $LINK -o bin/server
 fi
